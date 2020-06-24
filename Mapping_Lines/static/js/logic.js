af8709d81a7d4d8caa15e82eef782ed163d021f9
Duegan24 <62625959+Duegan24@users.noreply.github.com>
2020-06-23 21:08:23 -0500

    all

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,9 +3,23 @@ console.log("working");
 
 // Create the map object with a center and zoom level - scale is from 0-18.
 let map = L.map("mapid", {
-    center: [34.0522, -100],
-    zoom: 4
+    center: [40.0522, -120],
+    zoom: 5
 });
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
 
 // Get data from cities.js
 let cityData = cities;
