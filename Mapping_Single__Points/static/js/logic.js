27cef6c001846ef6fb5cb8bb7d27eab2630491b6
Duegan24 <62625959+Duegan24@users.noreply.github.com>
2020-06-23 20:01:03 -0500

    all

--- Mapping_Single__Points/static/js/logic.js
+++ Mapping_Single__Points/static/js/logic.js
@@ -3,19 +3,27 @@ console.log("working");
 
 // Create the map object with a center and zoom level - scale is from 0-18.
 let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
+    center: [34.0522, -118.2437],
+    zoom: 14
 });
 
+// [40.7, -94.5]
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.circleMarker([34.0522, -118.2437], {
+  color: 'black',
+  fillColor: '#ffffa1',
+  radius: 300
+}).addTo(map);
 
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: mapbox_api_key
 });
+
+// map styles I like satellite-streets-v11
 
 // Then we add our 'graymap' tile layer to the map
 streets.addTo(map);
